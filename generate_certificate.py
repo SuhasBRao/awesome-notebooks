eeb9c76daa83567e31e623730353902e5ef59a56
@SuhasBRao@ <raos04567@gmail.com>
2022-10-12 19:18:06 +0530

    testing with repo_url

--- generate_certificate.py
+++ generate_certificate.py
@@ -1,5 +1,23 @@
 import os
 
-input_variable = os.environ['pr_name']
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+from bs4 import BeautifulSoup
 
-print("PR:", input_variable)
+
+org_profile_url = os.environ('org_profile_url')
+repo_name = "awesome-notebooks"
+date_of_contribution = "04/10/2022"
+contributor_name = "Suhas B"
+issue_name = "Pillow - Generate a certificate template #1212"
+issue_id = "1212"
+pr_name = os.environ['pr_name']
+
+# These are accessed later by few functions
+template_path = "certificate-template.png"
+default_font_path = "Roboto-Regular.ttf"
+content_font_path = "AnonymousPro-Regular.ttf"
+
+print("PR name:", pr_name)
+print("ORG url:", )
