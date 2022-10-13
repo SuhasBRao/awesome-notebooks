a73d0c27a2e9d80ca4ac76cad3b2591394303269
@SuhasBRao@ <raos04567@gmail.com>
2022-10-13 08:42:06 +0530

    added new variables

--- generate_certificate.py
+++ generate_certificate.py
@@ -6,19 +6,20 @@ from io import BytesIO
 from bs4 import BeautifulSoup
 
 
-org_profile_url = os.environ['org_profile_url']
+org_profile_url = os.environ["org_profile_url"]
 repo_name = os.environ["repo_name"]
-date_of_contribution = "04/10/2022"
+date_of_contribution = os.environ["date"]
 contributor_name = os.environ["contributor_name"]
-issue_name = "Pillow - Generate a certificate template #1212"
+issue_name = os.environ["issue_title"]
 issue_id = "1212"
-pr_name = os.environ['pr_name']
+pr_name = os.environ["pr_name"]
 
 # These are accessed later by few functions
 template_path = "certificate-template.png"
 default_font_path = "Roboto-Regular.ttf"
 content_font_path = "AnonymousPro-Regular.ttf"
 
+print("Inside Python:")
 print("PR name:", pr_name)
 print("ORG url:", org_profile_url)
 print("Repo name", repo_name)
